d66e6ea43194448effc90594fed52084c14558b9
musta <musta@ASUS-MUSTAFA.mshome.net>
2021-05-23 17:14:51 +0300

    Finish V1

--- Rock-Paper-Scissors/src/Helper/myOwnKeyListener.java
+++ Rock-Paper-Scissors/src/Helper/myOwnKeyListener.java
@@ -1,30 +1,280 @@
 package Helper;
 
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.sql.Time;
+import java.time.zone.ZoneOffsetTransitionRule.TimeDefinition;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-public class myOwnKeyListener implements KeyListener{
+import javax.swing.ImageIcon;
+
+import MainPage.MainScreenGUI;
+import jdk.jfr.Timespan;
+
+public class myOwnKeyListener implements KeyListener {
+
+	boolean A = false;
+	boolean S = false;
+	boolean D = false;
+	boolean J = false;
+	boolean K = false;
+	boolean L = false;
+	boolean start = true;
+	static CountDownLatch latch = new CountDownLatch(3);
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		 System.out.println("Key Pressed " + KeyEvent.getKeyText(e.getKeyCode()));
-         if("Right".equals(KeyEvent.getKeyText(e.getKeyCode())))
-            System.out.println("Right Button Detected");
-        /* if(e.getKeyCode()==KeyEvent.VK_ESCAPE)
-            System.exit(0);		*/
+
+		/*
+		 * for(int i = 3;i>=0;i--) { try { TimeUnit.SECONDS.sleep(1); } catch
+		 * (InterruptedException e1) { // TODO Auto-generated catch block
+		 * e1.printStackTrace(); } }
+		 */
+		if (start) {
+
+			if (e.getKeyChar() == 'A' || e.getKeyChar() == 'a') {
+				ImageIcon imageIconL = new ImageIcon(MainScreenGUI.class.getResource("/Images/rockL.png"));
+				Image newImageL = imageIconL.getImage();
+				Image imageL = newImageL.getScaledInstance(308, 247, Image.SCALE_SMOOTH);
+				MainScreenGUI.lbl_Left.setIcon(new ImageIcon(imageL));
+			}
+
+			if (e.getKeyChar() == 'J' || e.getKeyChar() == 'j') {
+				ImageIcon imageIconR = new ImageIcon(MainScreenGUI.class.getResource("/Images/rockR.png"));
+				Image newImageR = imageIconR.getImage();
+				Image imageR = newImageR.getScaledInstance(308, 247, Image.SCALE_SMOOTH);
+				MainScreenGUI.lbl_Right.setIcon(new ImageIcon(imageR));
+			}
+
+			if (e.getKeyChar() == 'S' || e.getKeyChar() == 's') {
+				ImageIcon imageIconL = new ImageIcon(MainScreenGUI.class.getResource("/Images/paperL.png"));
+				Image newImageL = imageIconL.getImage();
+				Image imageL = newImageL.getScaledInstance(308, 247, Image.SCALE_SMOOTH);
+				MainScreenGUI.lbl_Left.setIcon(new ImageIcon(imageL));
+			}
+
+			if (e.getKeyChar() == 'K' || e.getKeyChar() == 'k') {
+				ImageIcon imageIconR = new ImageIcon(MainScreenGUI.class.getResource("/Images/paperR.png"));
+				Image newImageR = imageIconR.getImage();
+				Image imageR = newImageR.getScaledInstance(308, 247, Image.SCALE_SMOOTH);
+				MainScreenGUI.lbl_Right.setIcon(new ImageIcon(imageR));
+			}
+
+			if (e.getKeyChar() == 'D' || e.getKeyChar() == 'd') {
+				ImageIcon imageIconL = new ImageIcon(MainScreenGUI.class.getResource("/Images/scissorsL.png"));
+				Image newImageL = imageIconL.getImage();
+				Image imageL = newImageL.getScaledInstance(308, 247, Image.SCALE_SMOOTH);
+				MainScreenGUI.lbl_Left.setIcon(new ImageIcon(imageL));
+			}
+
+			if (e.getKeyChar() == 'L' || e.getKeyChar() == 'l') {
+				ImageIcon imageIconR = new ImageIcon(MainScreenGUI.class.getResource("/Images/scissorsR.png"));
+				Image newImageR = imageIconR.getImage();
+				Image imageR = newImageR.getScaledInstance(308, 247, Image.SCALE_SMOOTH);
+				MainScreenGUI.lbl_Right.setIcon(new ImageIcon(imageR));
+			}
+			// Score <<<<<<<<<<<<<
+			/*
+			 * LEFT PRESS TO KEY RIGHT PRESS TO KEY (e.getKeyChar() == 'A' || e.getKeyChar()
+			 * == 'a') Rock (e.getKeyChar() == 'J' || e.getKeyChar() == 'j')
+			 * 
+			 * (e.getKeyChar() == 'S' || e.getKeyChar() == 's') Paper (e.getKeyChar() == 'K'
+			 * || e.getKeyChar() == 'k')
+			 * 
+			 * (e.getKeyChar() == 'D' || e.getKeyChar() == 'd') Scissors (e.getKeyChar() ==
+			 * 'L' || e.getKeyChar() == 'l')
+			 * 
+			 *
+			 * Rock > Scissors Scissors > Paper Paper > Rock
+			 */
+
+			if ((e.getKeyChar() == 'A' || e.getKeyChar() == 'a')) {
+				A = true;
+				S = false;
+				D = false;
+
+			}
+			if ((e.getKeyChar() == 'S' || e.getKeyChar() == 's')) {
+				S = true;
+				A = false;
+				D = false;
+			}
+			if ((e.getKeyChar() == 'D' || e.getKeyChar() == 'd')) {
+				D = true;
+				A = false;
+				S = false;
+			}
+			if ((e.getKeyChar() == 'J' || e.getKeyChar() == 'j')) {
+				J = true;
+				K = false;
+				L = false;
+			}
+			if ((e.getKeyChar() == 'K' || e.getKeyChar() == 'k')) {
+				K = true;
+				J = false;
+				L = false;
+			}
+			if ((e.getKeyChar() == 'L' || e.getKeyChar() == 'l')) {
+				L = true;
+				K = false;
+				J = false;
+			}
+			// Left Win
+			if (A && L) {
+				int score = Integer.parseInt(MainScreenGUI.lbl_LeftScore.getText());
+				MainScreenGUI.lbl_Result.setText("Pink Win!");
+				score = score + 1;
+				MainScreenGUI.lbl_LeftScore.setText(score + "");
+				A = false;
+				S = false;
+				D = false;
+				J = false;
+				K = false;
+				L = false;
+				start = false;
+			}
+			if (S && J) {
+				int score = Integer.parseInt(MainScreenGUI.lbl_LeftScore.getText());
+				MainScreenGUI.lbl_Result.setText("Pink Win!");
+				score = score + 1;
+				MainScreenGUI.lbl_LeftScore.setText(score + "");
+				A = false;
+				S = false;
+				D = false;
+				J = false;
+				K = false;
+				L = false;
+				start = false;
+			}
+			if (D && K) {
+				int score = Integer.parseInt(MainScreenGUI.lbl_LeftScore.getText());
+				MainScreenGUI.lbl_Result.setText("Pink Win!");
+				score = score + 1;
+				MainScreenGUI.lbl_LeftScore.setText(score + "");
+				A = false;
+				S = false;
+				D = false;
+				J = false;
+				K = false;
+				L = false;
+				start = false;
+			}
+			// Right Win
+			if (J && D) {
+				int score = Integer.parseInt(MainScreenGUI.lbl_RightScore.getText());
+				MainScreenGUI.lbl_Result.setText("Blue Win!");
+				score = score + 1;
+				MainScreenGUI.lbl_RightScore.setText(score + "");
+				A = false;
+				S = false;
+				D = false;
+				J = false;
+				K = false;
+				L = false;
+				start = false;
+			}
+			if (K && A) {
+				int score = Integer.parseInt(MainScreenGUI.lbl_RightScore.getText());
+				MainScreenGUI.lbl_Result.setText("Blue Win!");
+				score = score + 1;
+				MainScreenGUI.lbl_RightScore.setText(score + "");
+				A = false;
+				S = false;
+				D = false;
+				J = false;
+				K = false;
+				L = false;
+				start = false;
+			}
+			if (L && S) {
+				int score = Integer.parseInt(MainScreenGUI.lbl_RightScore.getText());
+				MainScreenGUI.lbl_Result.setText("Blue Win!");
+				score = score + 1;
+				MainScreenGUI.lbl_RightScore.setText(score + "");
+				A = false;
+				S = false;
+				D = false;
+				J = false;
+				K = false;
+				L = false;
+
+				start = false;
+			}
+			// Draw
+			if (A && J) {
+				MainScreenGUI.lbl_Result.setText("DRAW");
+				A = false;
+				S = false;
+				D = false;
+				J = false;
+				K = false;
+				L = false;
+				start = false;
+			}
+			if (S && K) {
+				MainScreenGUI.lbl_Result.setText("DRAW");
+				A = false;
+				S = false;
+				D = false;
+				J = false;
+				K = false;
+				L = false;
+				start = false;
+			}
+			if (D && L) {
+				MainScreenGUI.lbl_Result.setText("DRAW");
+				A = false;
+				S = false;
+				D = false;
+				J = false;
+				K = false;
+				L = false;
+				start = false;
+			}
+
+		}
+		if (e.getKeyChar() == KeyEvent.VK_C || e.getKeyChar() == 'c') {
+			start = true;
+			A = false;
+			S = false;
+			D = false;
+			J = false;
+			K = false;
+			L = false;
+			MainScreenGUI.lbl_Left.setIcon(null);
+			MainScreenGUI.lbl_Right.setIcon(null);
+			MainScreenGUI.lbl_Result.setText("");
+		}
+		if (e.getKeyChar() == KeyEvent.VK_R || e.getKeyChar() == 'r') {
+			start = true;
+			A = false;
+			S = false;
+			D = false;
+			J = false;
+			K = false;
+			L = false;
+			MainScreenGUI.lbl_Left.setIcon(null);
+			MainScreenGUI.lbl_Right.setIcon(null);
+			MainScreenGUI.lbl_LeftScore.setText("0");
+			MainScreenGUI.lbl_RightScore.setText("0");
+			MainScreenGUI.lbl_Result.setText("");
+		}
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
-	
 }
--- Rock-Paper-Scissors/src/MainPage/MainScreenGUI.java
+++ Rock-Paper-Scissors/src/MainPage/MainScreenGUI.java
@@ -15,13 +15,26 @@ import java.awt.Font;
 import java.awt.Image;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.Icon;
 import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MainScreenGUI extends JFrame {
 
 	private JPanel contentPane;
+	public static JLabel lbl_Left;
+	public static JLabel lbl_Right;
+	public static JLabel lbl_LeftScore;
+	public static JLabel lbl_RightScore;
+	public static JLabel lbl_Clean;
+	public static JLabel lbl_Result;
+
 
 	/**
 	 * Launch the application.
@@ -31,6 +44,8 @@ public class MainScreenGUI extends JFrame {
 			public void run() {
 				try {
 					MainScreenGUI frame = new MainScreenGUI();
+					myOwnKeyListener keyboard = new myOwnKeyListener();
+					frame.addKeyListener(keyboard);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -43,134 +58,176 @@ public class MainScreenGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public MainScreenGUI() {
+		setResizable(false);
 		setTitle("Rock-Paper-Scissors");
-		
-		JLabel lbl_Icon1;
-		JLabel lbl_Icon2;
-		JLabel lbl_Icon3;
-		
-	
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(300, 100, 700, 600);
 		contentPane = new JPanel();
-		myOwnKeyListener keyboard = new myOwnKeyListener();
+
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lbl_1 = new JLabel("A :");
 		lbl_1.setFont(new Font("Yu Gothic UI Semilight", Font.BOLD, 25));
 		lbl_1.setBounds(20, 449, 46, 24);
 		contentPane.add(lbl_1);
-		
+
 		JLabel lbl_2 = new JLabel("S :");
 		lbl_2.setFont(new Font("Yu Gothic UI Semilight", Font.BOLD, 25));
 		lbl_2.setBounds(24, 484, 46, 24);
 		contentPane.add(lbl_2);
-		
+
 		JLabel lbl_3 = new JLabel("D :");
 		lbl_3.setFont(new Font("Yu Gothic UI Semilight", Font.BOLD, 25));
 		lbl_3.setBounds(20, 519, 46, 24);
 		contentPane.add(lbl_3);
-		
-		ImageIcon imageIcon1= new ImageIcon(MainScreenGUI.class.getResource("/Images/rockL.png"));
+
+		ImageIcon imageIcon1 = new ImageIcon(MainScreenGUI.class.getResource("/Images/rockL.png"));
 		Image newImage1 = imageIcon1.getImage();
-		Image image1= newImage1.getScaledInstance(46, 31, Image.SCALE_SMOOTH);
-		
-		lbl_Icon1 = new JLabel(new ImageIcon(image1));
+		Image image1 = newImage1.getScaledInstance(46, 31, Image.SCALE_SMOOTH);
+
+		JLabel lbl_Icon1 = new JLabel(new ImageIcon(image1));
 		lbl_Icon1.setBounds(60, 449, 46, 31);
 		contentPane.add(lbl_Icon1);
-		
-		ImageIcon imageIcon2= new ImageIcon(MainScreenGUI.class.getResource("/Images/paperL.png"));
-		Image newImage2=imageIcon2.getImage();
-		Image image2= newImage2.getScaledInstance(46, 31, Image.SCALE_SMOOTH);
-		
-		lbl_Icon2 = new JLabel(new ImageIcon(image2));
+
+		ImageIcon imageIcon2 = new ImageIcon(MainScreenGUI.class.getResource("/Images/paperL.png"));
+		Image newImage2 = imageIcon2.getImage();
+		Image image2 = newImage2.getScaledInstance(46, 31, Image.SCALE_SMOOTH);
+
+		JLabel lbl_Icon2 = new JLabel(new ImageIcon(image2));
 		lbl_Icon2.setBounds(60, 484, 46, 31);
 		contentPane.add(lbl_Icon2);
-		
 
-		ImageIcon imageIcon3= new ImageIcon(MainScreenGUI.class.getResource("/Images/scissorsL.png"));
-		Image newImage3=imageIcon3.getImage();
-		Image image3= newImage3.getScaledInstance(46, 31, Image.SCALE_SMOOTH);
-		
-		lbl_Icon3 = new JLabel(new ImageIcon(image3));
+		ImageIcon imageIcon3 = new ImageIcon(MainScreenGUI.class.getResource("/Images/scissorsL.png"));
+		Image newImage3 = imageIcon3.getImage();
+		Image image3 = newImage3.getScaledInstance(46, 31, Image.SCALE_SMOOTH);
+
+		JLabel lbl_Icon3 = new JLabel(new ImageIcon(image3));
 		lbl_Icon3.setBounds(60, 519, 46, 31);
 		contentPane.add(lbl_Icon3);
-		
+
 		JLabel lbl_4 = new JLabel(": J");
 		lbl_4.setFont(new Font("Yu Gothic UI Semilight", Font.BOLD, 25));
 		lbl_4.setBounds(637, 456, 46, 24);
 		contentPane.add(lbl_4);
-		
+
 		JLabel lbl_5 = new JLabel(": K");
 		lbl_5.setFont(new Font("Yu Gothic UI Semilight", Font.BOLD, 25));
 		lbl_5.setBounds(637, 491, 46, 24);
 		contentPane.add(lbl_5);
-		
+
 		JLabel lbl_6 = new JLabel(": L");
 		lbl_6.setFont(new Font("Yu Gothic UI Semilight", Font.BOLD, 25));
 		lbl_6.setBounds(638, 526, 46, 24);
 		contentPane.add(lbl_6);
-		
-		ImageIcon imageIcon4= new ImageIcon(MainScreenGUI.class.getResource("/Images/rockR.png"));
+
+		ImageIcon imageIcon4 = new ImageIcon(MainScreenGUI.class.getResource("/Images/rockR.png"));
 		Image newImage4 = imageIcon4.getImage();
-		Image image4= newImage4.getScaledInstance(46, 31, Image.SCALE_SMOOTH);
-		
+		Image image4 = newImage4.getScaledInstance(46, 31, Image.SCALE_SMOOTH);
+
 		JLabel lbl_Icon4 = new JLabel(new ImageIcon(image4));
 		lbl_Icon4.setBounds(581, 449, 46, 31);
 		contentPane.add(lbl_Icon4);
-		
-		ImageIcon imageIcon5= new ImageIcon(MainScreenGUI.class.getResource("/Images/paperR.png"));
+
+		ImageIcon imageIcon5 = new ImageIcon(MainScreenGUI.class.getResource("/Images/paperR.png"));
 		Image newImage5 = imageIcon5.getImage();
-		Image image5= newImage5.getScaledInstance(46, 31, Image.SCALE_SMOOTH);
-		
-		
+		Image image5 = newImage5.getScaledInstance(46, 31, Image.SCALE_SMOOTH);
+
 		JLabel lbl_Icon5 = new JLabel(new ImageIcon(image5));
 		lbl_Icon5.setBounds(581, 484, 46, 31);
 		contentPane.add(lbl_Icon5);
-		
-		ImageIcon imageIcon6= new ImageIcon(MainScreenGUI.class.getResource("/Images/scissorsR.png"));
+
+		ImageIcon imageIcon6 = new ImageIcon(MainScreenGUI.class.getResource("/Images/scissorsR.png"));
 		Image newImage6 = imageIcon6.getImage();
-		Image image6= newImage6.getScaledInstance(46, 31, Image.SCALE_SMOOTH);
-		
+		Image image6 = newImage6.getScaledInstance(46, 31, Image.SCALE_SMOOTH);
+
 		JLabel lbl_Icon6 = new JLabel(new ImageIcon(image6));
 		lbl_Icon6.setBounds(581, 519, 46, 31);
 		contentPane.add(lbl_Icon6);
-		
+
 		JPanel w_pane = new JPanel();
-		w_pane.setBackground(new Color(255, 255, 255));
+		w_pane.setBackground(new Color(250, 250, 210));
 		w_pane.setBounds(0, 0, 684, 438);
 		contentPane.add(w_pane);
 		w_pane.setLayout(null);
-		
-		JLabel lbl_Left = new JLabel("");
+
+		lbl_Left = new JLabel("");
 		lbl_Left.setBounds(10, 80, 308, 247);
 		w_pane.add(lbl_Left);
+
+		lbl_Right = new JLabel("");
+		lbl_Right.setBounds(366, 80, 308, 247);
+		w_pane.add(lbl_Right);
 		
-		JLabel lbl_right = new JLabel("");
-		lbl_right.setBounds(366, 80, 308, 247);
-		w_pane.add(lbl_right);
-	
-		contentPane.addKeyListener(keyboard);
+		lbl_Result = new JLabel("");
+		lbl_Result.setBackground(new Color(211, 211, 211));
+		lbl_Result.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_Result.setForeground(new Color(0, 0, 0));
+		lbl_Result.setFont(new Font("Tahoma", Font.BOLD, 60));
+		lbl_Result.setBounds(10, 7, 664, 101);
+		w_pane.add(lbl_Result);
+
+		JLabel lbl_Duo = new JLabel(":");
+		lbl_Duo.setFont(new Font("Tahoma", Font.BOLD, 50));
+		lbl_Duo.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_Duo.setBounds(320, 456, 46, 87);
+		contentPane.add(lbl_Duo);
+
+		lbl_LeftScore = new JLabel("0");
+		lbl_LeftScore.setForeground(new Color(255, 182, 193));
+		lbl_LeftScore.setFont(new Font("Tahoma", Font.BOLD, 60));
+		lbl_LeftScore.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_LeftScore.setBounds(158, 449, 131, 101);
+		contentPane.add(lbl_LeftScore);
+
+		lbl_RightScore = new JLabel("0");
+		lbl_RightScore.setForeground(new Color(0, 255, 255));
+		lbl_RightScore.setFont(new Font("Tahoma", Font.BOLD, 60));
+		lbl_RightScore.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_RightScore.setBounds(401, 449, 131, 101);
+		contentPane.add(lbl_RightScore);
+
+		JPanel w_paneRight = new JPanel();
+		w_paneRight.setBackground(new Color(0, 139, 139));
+		w_paneRight.setBounds(401, 449, 131, 101);
+		contentPane.add(w_paneRight);
+
+		JPanel w_paneLeft = new JPanel();
+		w_paneLeft.setBackground(new Color(0, 139, 139));
+		w_paneLeft.setBounds(158, 449, 131, 101);
+		contentPane.add(w_paneLeft);
 		
-	/*	lbl_Left.addKeyListener(new KeyAdapter() {
-		@Override
-		public void keyPressed(KeyEvent e) {
-			if(e.getKeyChar()=='A' || e.getKeyChar()=='a')
-			{
-				System.out.println("sa");
-				ImageIcon imageIconL= new ImageIcon(MainScreenGUI.class.getResource("/Images/scissorsR.png"));
-				Image newImageL = imageIconL.getImage();
-				Image imageL= newImageL.getScaledInstance(308,247, Image.SCALE_SMOOTH);
-				lbl_Left.setIcon(new ImageIcon(imageL));
+		lbl_Clean = new JLabel("Clean C");
+		lbl_Clean.setForeground(new Color(138, 43, 226));
+		lbl_Clean.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lbl_Clean.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_Clean.setBounds(299, 449, 92, 31);
+		contentPane.add(lbl_Clean);
+		
+		JLabel lbl_Reset = new JLabel("Reset R");
+		lbl_Reset.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_Reset.setForeground(new Color(138, 43, 226));
+		lbl_Reset.setFont(new Font("Tahoma", Font.BOLD, 18));
+		lbl_Reset.setBounds(299, 519, 92, 31);
+		contentPane.add(lbl_Reset);
+		/*
+		JButton btn_Start = new JButton("Start");
+		btn_Start.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 			}
-			
-			
-			super.keyPressed(e);
-		}
+		});
+		btn_Start.setForeground(new Color(60, 179, 113));
+		btn_Start.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btn_Start.setBounds(302, 449, 89, 23);
+		contentPane.add(btn_Start);
 		
-		});*/
+		JButton btn_Exit = new JButton("Exit");
+		btn_Exit.setForeground(new Color(255, 0, 0));
+		btn_Exit.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btn_Exit.setBounds(302, 526, 89, 23);
+		contentPane.add(btn_Exit);*/
 		
 		
 	}
